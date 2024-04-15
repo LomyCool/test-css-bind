00b27bec58b559015db68a95ef29c9df6a489bc8
lomyo <haojia.wu@southgis.com>
2024-04-15 18:25:56 +0800

    ..

---
+++ docs/quickstart.md
@@ -0,0 +1,17 @@
+# 快速上手
+
+### 安装
+
+```bash
+# 通过 npm
+npm i test-css-bind
+
+# 通过 yarn
+yarn add test-css-bind
+
+# 通过 pnpm
+pnpm add test-css-bind
+
+# 通过 bun
+bun add test-css-bind
+```
---
+++ src/demo-button/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1 class="demo1">12312312</h1>
+    <h2>DDDDDD</h2>
+  <button class="demo-button">
+    <slot />
+  </button>
+
+ </template>
+ <script setup lang="ts">
+   const width = `100px`
+   const height = `100px`
+ </script>
+ 
+ <style scoped>
+ 
+ </style>
+
+<style lang="less">
+
+.demo1 {
+  width: v-bind(width);
+  height: v-bind(height);
+  background-color: #f44;
+}
+.demo-button {
+  min-width: 120px;
+  color: #fff;
+  font-size: 16px;
+  line-height: 36px;
+  background-color: #f44;
+  border: none;
+  border-radius: 30px;
+}
+</style>
