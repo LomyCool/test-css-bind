f9b95479bc6d1faef343991e4c98190dafaef886
lomyo <haojia.wu@southgis.com>
2024-04-29 11:54:20 +0800

    bug: Selector "[data-v-30d142e3]" can't have a suffix

---
+++ src/comp-two/comp2.vue
@@ -0,0 +1,16 @@
+
+<script lang="ts">
+export default {
+  name: 'CompTwo',
+};
+</script>
+<style lang="scss" scoped>
+ 
+ // 有注释打包不会报错，但没生成css文件
+ .describe-item {
+      &-text {
+        word-break: keep-all;
+      }
+    }
+ 
+</style>
